3efda87712b8c0f0fa8fa67df67fb87b09201128
crowddevelopers08-arch <crowddevelopers08@gmail.com>
2026-07-17 13:43:48 +0530

    skinsa lp added meta pixel code today

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { DM_Serif_Display, Source_Sans_3, Work_Sans } from "next/font/google";
 import Script from "next/script";
 import "./globals.css";
+
 import Navbar from "@/component/navbar";
 import ScrollToTop from "@/component/scroll-to-top";
 import Footer from "@/component/footer";
@@ -36,11 +37,25 @@ export const metadata: Metadata = {
       { url: "/logo-skinsa.jpg", sizes: "32x32", type: "image/png" },
     ],
     apple: [
-      { url: "/logo-skinsa.jpg", sizes: "180x180", type: "image/png" },
+      {
+        url: "/logo-skinsa.jpg",
+        sizes: "180x180",
+        type: "image/png",
+      },
     ],
     other: [
-      { rel: "icon", url: "/logo-skinsa.jpg", sizes: "192x192", type: "image/png" },
-      { rel: "icon", url: "/logo-skinsa.jpg", sizes: "512x512", type: "image/png" },
+      {
+        rel: "icon",
+        url: "/logo-skinsa.jpg",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        rel: "icon",
+        url: "/logo-skinsa.jpg",
+        sizes: "512x512",
+        type: "image/png",
+      },
     ],
   },
 };
@@ -51,13 +66,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={`${dmSerif.variable} ${workSans.variable} ${sourceSans.variable} h-full antialiased`}>
+    <html
+      lang="en"
+      className={`${dmSerif.variable} ${workSans.variable} ${sourceSans.variable} h-full antialiased`}
+    >
       <body className="min-h-full flex flex-col font-[family-name:var(--font-work-sans)]">
-        {/* Google Analytics */}
+        {/* Google Ads Tag */}
         <Script
-          src={`https://www.googletagmanager.com/gtag/js?id=AW-17635706476`}
+          src="https://www.googletagmanager.com/gtag/js?id=AW-17635706476"
           strategy="afterInteractive"
         />
+
+        {/* Google Ads Configuration */}
         <Script id="google-analytics" strategy="afterInteractive">
           {`
             window.dataLayer = window.dataLayer || [];
@@ -66,9 +86,46 @@ export default function RootLayout({
             gtag('config', 'AW-17635706476');
           `}
         </Script>
-        
+
+        {/* Facebook Meta Pixel */}
+        <Script id="facebook-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;
+            n.push=n;
+            n.loaded=!0;
+            n.version='2.0';
+            n.queue=[];
+            t=b.createElement(e);
+            t.async=!0;
+            t.src=v;
+            s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}
+            (window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+
+            fbq('init', '27154693444226014');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+
+        {/* Meta Pixel NoScript Fallback */}
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=27154693444226014&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
+
+        {/* Optional Components */}
         {/* <ScrollToTop /> */}
         <RevealObserver />
+
         {children}
       </body>
     </html>
